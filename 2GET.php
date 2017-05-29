499ebd6ce3a887234732ab4db2d09383db746b3a
SoniaVarian <soniavalmai15@gmail.com>
2017-05-29 15:25:06 +0700

    Add files via upload

---
+++ 2GET.php
@@ -0,0 +1,9 @@
+
+
+
+<?php
+if(isset($_GET['send']))
+{
+echo"Welcome ".$_GET["name"] ."<br>";
+echo"You are ".$_GET["age"]." years old!";
+}
---
+++ 2POST.php
@@ -0,0 +1,7 @@
+
+
+if(isset($_POST['send']))
+{
+echo"Welcome ".$_POST["name"] ."<br>";
+echo"You are ".$_POST["age"]." years old!";
+}
